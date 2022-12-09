f51c086267834b2dbcae8c7cb5f037bef17a9843
Jakob Rolandsson <jakobrolandsson@Jakobs-Air.local>
2022-12-09 15:44:02 +0100

    base commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let express = require('express');
+let app = express();
+let cors = require('cors')
+let server = require('http').createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+let port = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: '*'
+}));
+
+
+let rooms = []
+
+app.get("/room", (req, res) => {
+  res.send(rooms[req.body]);
+});
+
+app.post("/room", (req, res) => {
+  rooms.push(req.body);
+  res.json(rooms.length + 1000);
+});
+
+io.on("connection", socket => {
+  socket.on("tic-update", room => {
+    rooms[room.id - 1000] = room.board;
+    io.emit("tic-update-" + room.id, { id: room.id, board: rooms[room.id - 1000] });
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
